3f6cf7999e73e836e943fa04a5913cfdce14784a
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2026-01-05 22:50:19 +0100

    refactor:  Centralized common interfaces in globe.interface.ts

--- lib/interfaces/globe.interface.ts
+++ lib/interfaces/globe.interface.ts
@@ -1,4 +1,6 @@
+import { DisplaySettings } from '@/components/sections/home/SettingsModal';
 import * as THREE from 'three';
+import { Article } from './article.interface';
 
 export interface GlobeControls {
     target: THREE.Vector3;
@@ -7,4 +9,38 @@ export interface GlobeControls {
     reset: () => void;
     minDistance: number;
     maxDistance: number;
+}
+
+export interface GlobeProps {
+    filters: {
+        search: string;
+        yearRange: [number, number];
+        categories: string[];
+        eventTypes: string[];
+        regions: string[];
+    };
+    isPaused: boolean;
+    settings: {
+        visual: {
+            rotationSpeed: number;
+            showEffects: boolean;
+            clusterThreshold?: number;
+        };
+        display: DisplaySettings;
+    };
+}
+
+export interface ClusterGroup {
+    center: [number, number, number];
+    markers: {
+        article: Article;
+        position: [number, number, number];
+    }[];
+    isVisible?: boolean;
+}
+
+export interface MarkersLayerProps {
+    clusterGroups: ClusterGroup[];
+    onHover: (isHovered: boolean) => void;
+    cameraDistance: number;
 }

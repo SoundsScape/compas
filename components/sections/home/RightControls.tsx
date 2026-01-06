f3cf2d486b524fcd442f9ad62963e16dae924cca
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2026-01-06 23:07:47 +0100

    refactor: style and positioning of RightControls.ts

--- components/sections/home/RightControls.tsx
+++ components/sections/home/RightControls.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { Settings } from 'lucide-react';
 
 interface RightControlsProps {
@@ -12,27 +13,26 @@ export function RightControls({
     handleSettingsClick,
 }: RightControlsProps) {
     return (
-        <>
-            {/* Botón de Ajustes */}
-            <div className="pointer-events-auto absolute right-4 bottom-10/24 z-20 flex -translate-y-15/20 flex-col gap-2">
-                <button
-                    onClick={handleSettingsClick}
-                    className="cursor-pointer rounded-full bg-black/80 p-2 text-white backdrop-blur-md transition-all duration-300 hover:bg-black/90"
-                >
-                    <Settings className="h-5 w-5" />
-                </button>
-            </div>
-
+        <div className="absolute top-20 right-4 z-20 flex flex-col items-end gap-2">
             {/* Switch de Map/Globe */}
-            <label className="pointer-events-auto absolute top-23 right-20 z-20 mb-4 inline-flex cursor-pointer items-center">
+            <label className="pointer-events-auto z-20 mb-4 inline-flex cursor-pointer items-center">
                 <input
                     type="checkbox"
                     className="peer sr-only"
                     checked={showMap}
                     onChange={() => setShowMap(!showMap)}
                 />
-                <div className="h-8 w-14 rounded-full bg-gray-300 peer-checked:bg-blue-500 peer-focus:ring-2 peer-focus:ring-blue-300 before:absolute before:top-1 before:left-1 before:h-6 before:w-6 before:rounded-full before:bg-white before:transition-transform before:content-[''] peer-checked:before:translate-x-6"></div>
+                <div className="peer-checked:bg-primary/90 h-8 w-14 rounded-full bg-gray-400 peer-focus:ring-2 peer-focus:ring-blue-300 before:absolute before:top-1 before:left-1 before:h-6 before:w-6 before:rounded-full before:bg-white before:transition-transform before:content-[''] peer-checked:before:translate-x-6"></div>
             </label>
-        </>
+            {/* Botón de Ajustes */}
+            <div className="pointer-events-auto z-20">
+                <Button
+                    onClick={handleSettingsClick}
+                    className="hover:text-accent size-11"
+                >
+                    <Settings className="size-5.5" />
+                </Button>
+            </div>
+        </div>
     );
 }

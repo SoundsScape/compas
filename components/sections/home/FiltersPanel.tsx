5e542848d80066a85d570f6a297aa1571f56ab10
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2026-01-07 12:08:39 +0100

    feat: improve UI with responsive FiltersPanel

--- components/sections/home/FiltersPanel.tsx
+++ components/sections/home/FiltersPanel.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import Filters from './Filters';
+import { Button } from '@/components/ui/button';
 
 interface FiltersPanelProps {
     showFilters: boolean;
@@ -22,21 +23,22 @@ export function FiltersPanel({
 }: FiltersPanelProps) {
     return (
         <div
-            className={`pointer-events-auto fixed top-20 left-4 z-10 flex items-start gap-1 transition-all duration-300 ${showFilters ? '-translate-x-[280px]' : 'translate-x-0'}`}
+            className={`pointer-events-auto fixed top-20 left-4 z-10 flex items-start gap-1 transition-all duration-300 ${showFilters ? '-translate-x-[290px] lg:-translate-x-[370px] 2xl:-translate-x-[440px]' : 'translate-x-0'}`}
         >
+            <div className={` transition-all duration-400 ${showFilters ? 'opacity-0' : 'opacity-100'}`}>
             <Filters
                 filters={filters}
                 setFilters={setFilters}
                 setSelectedYearRange={setSelectedYearRange}
                 minYear={minYear}
                 maxYear={maxYear}
-            />
-            <button
+            /></div>
+            <Button
                 onClick={() => setShowFilters(!showFilters)}
-                className={`bg-primary hover:text-accent z-20 rounded-md p-2 text-white backdrop-blur-md`}
+                className={`hover:text-accent z-20 size-11`}
             >
-                {showFilters ? <ChevronRight /> : <ChevronLeft />}
-            </button>
+                {showFilters ? <ChevronRight  className="size-5.5" /> : <ChevronLeft className="size-5.5" />}
+            </Button>
         </div>
     );
 }

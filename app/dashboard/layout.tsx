099479efe2638adab110c548cb27b0098bee1616
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2026-02-27 13:17:27 +0100

    feat: dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,41 @@
+import { AppSidebar } from "@/components/sections/dashboard/sidebar/AppSidebar"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbList,
+    BreadcrumbPage,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Panel de Control</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4 md:p-6">
+                    {children}
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    );
+}

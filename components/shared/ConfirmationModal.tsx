ec9fea4644d907b8a5a58f0552456e458b32ca81
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2026-03-02 10:04:14 +0100

    feat: add ConfirmationModal

---
+++ components/shared/ConfirmationModal.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+export function ConfirmationModal({
+    children,
+    title,
+    action,
+    description,
+    onConfirm,
+    variant = "destructive",
+    confirmClassName = ""
+}: {
+    children: React.ReactNode,
+    title: string,
+    action: string,
+    description: string,
+    onConfirm: () => void,
+    variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link",
+    confirmClassName?: string
+}) {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent className="">
+                <DialogHeader>
+                    <DialogTitle className="text-2xl mb-3">{title}</DialogTitle>
+                    <DialogDescription>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="gap-2">
+                    <DialogClose asChild>
+                        <Button variant="ghost" className="cursor-pointer">Cancelar</Button>
+                    </DialogClose>
+                    <Button
+                        type="submit"
+                        variant={variant}
+                        onClick={onConfirm}
+                        className={confirmClassName}
+                    >
+                        {action}
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}

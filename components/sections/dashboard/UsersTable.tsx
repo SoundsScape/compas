2dcf4440d3e5dba9cb650753de0933ca29fdff6f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2026-03-08 16:45:03 +0100

    feat: add UsersTable component

---
+++ components/sections/dashboard/UsersTable.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Badge } from "@/components/ui/badge"
+import UserActionsMenu from "@/components/shared/UserActionsMenu"
+import Link from "next/link"
+
+interface User {
+    id: number
+    username: string
+    email: string
+    school: string
+    role: string
+    status: string
+}
+
+interface UsersTableProps {
+    users: User[];
+    onEdit: (user: any) => void;
+    onDeleteSuccess: () => Promise<void>;
+}
+
+export function UsersTable({ users, onEdit, onDeleteSuccess }: UsersTableProps) {
+    return (
+        <div className="relative rounded-md border bg-background/80 backdrop-blur-xl overflow-x-auto custom-scrollbar w-full">
+            <Table >
+                <TableHeader className="h-16 text-xl bg-linear-to-r from-primary to-transparent">
+                    <TableRow className="hover:bg-transparent border">
+                        <TableHead className="text-center w-16">ID</TableHead>
+                        <TableHead className="w-16 md:w-32 truncate">Usuario</TableHead>
+                        <TableHead className="max-w-28 truncate">Email</TableHead>
+                        <TableHead className="max-w-28 md:max-w-32 truncate">Centro Educativo</TableHead>
+                        <TableHead className="text-center w-24">Rol</TableHead>
+                        <TableHead className="text-center w-24">Estado</TableHead>
+                        <TableHead className="text-center w-16"></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {users.map((user) => (
+                        <TableRow key={user.id} className="border hover:from-background hover:via-primary/80 hover:to-background hover:bg-linear-to-b transition-colors text-nowrap">
+                            <TableCell className="text-muted-foreground text-center">{user.id}</TableCell>
+                            <TableCell className="font-medium w-16 md:w-32 truncate">
+                                <Link href={`/dashboard/user-info/${user.username}`} className="hover:underline">
+                                    {user.username}
+                                </Link>
+                            </TableCell>
+                            <TableCell className="text-muted-foreground max-w-32 truncate">
+                                {user.email}
+                            </TableCell>
+                            <TableCell className="text-muted-foreground max-w-32 truncate">
+                                {user.school}
+                            </TableCell>
+                            <TableCell className="text-center">
+                                <Badge variant="outline" className="border-white/20 font-normal">
+                                    {user.role}
+                                </Badge>
+                            </TableCell>
+                            <TableCell className="text-center">
+                                <Badge
+                                    className={user.status === 'activo'
+                                        ? "bg-green-500 text-black min-w-16"
+                                        : "bg-red-500 text-white min-w-16"
+                                    }
+                                >
+                                    {user.status}
+                                </Badge>
+                            </TableCell>
+                            <TableCell className="text-right">
+                                <div className="flex justify-end gap-2">
+                                    <UserActionsMenu
+                                        userId={user.id}
+                                        onEdit={() => onEdit(user)}
+                                        onActionSuccess={onDeleteSuccess}
+                                    />
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    )
+}

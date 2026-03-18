a9022f012574379b9e1315eb74b24cc4550e8347
karyoli <karyoli.ie@gmail.com>
2026-03-18 19:47:46 +0100

    refactor: button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
                     'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
                 secondary:
                     'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-                ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+                ghost: 'hover:text-accent dark:hover:bg-accent/50 p-0',
                 link: 'text-primary underline-offset-4 hover:underline',
                 filterActive:
                     'bg-muted border-white/5 text-muted-foreground shadow-sm hover:bg-primary/80 border rounded-sm',
@@ -27,7 +27,7 @@ const buttonVariants = cva(
             size: {
                 default: 'h-9 px-4 py-2 has-[>svg]:px-3',
                 filter: 'h-auto rounded-sm px-2 py-1 text-xs',
-                sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+                sm: 'h-8 rounded-sm gap-1.5 px-3 has-[>svg]:px-2.5',
                 lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
                 icon: 'size-9',
                 'icon-sm': 'size-8',
